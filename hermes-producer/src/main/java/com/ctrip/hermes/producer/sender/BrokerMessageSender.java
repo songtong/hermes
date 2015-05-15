de99aa19428bab11d829cee9986b1a9b2594dd37
Leo Liang <liangjinhua@gmail.com>
2015-05-15 19:31:32 +0800

    reduce producer sender thread to single thread

--- hermes-producer/src/main/java/com/ctrip/hermes/producer/sender/BrokerMessageSender.java
+++ hermes-producer/src/main/java/com/ctrip/hermes/producer/sender/BrokerMessageSender.java
@@ -2,14 +2,20 @@ package com.ctrip.hermes.producer.sender;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.unidal.lookup.annotation.Named;
 import org.unidal.tuple.Pair;
@@ -28,91 +34,137 @@ import com.google.common.util.concurrent.SettableFuture;
 @Named(type = MessageSender.class, value = Endpoint.BROKER)
 public class BrokerMessageSender extends AbstractMessageSender implements MessageSender {
 
-	private ConcurrentMap<Pair<String, Integer>, ProducerEndpointWorker> m_workers = new ConcurrentHashMap<>();
+	private ConcurrentMap<Pair<String, Integer>, TaskQueue> m_taskQueues = new ConcurrentHashMap<>();
+
+	private AtomicBoolean m_workerStarted = new AtomicBoolean(false);
 
 	@Override
 	public Future<SendResult> doSend(ProducerMessage<?> msg) {
+		if (m_workerStarted.compareAndSet(false, true)) {
+			// TODO config
+			startEndpointSender(10, 50, 3000, 50L);
+		}
 
-		return createWorkerIfNecessary(msg.getTopic(), msg.getPartitionNo()).submit(msg);
+		Pair<String, Integer> tp = new Pair<String, Integer>(msg.getTopic(), msg.getPartitionNo());
+		// TODO queueSize
+		m_taskQueues.putIfAbsent(tp, new TaskQueue(msg.getTopic(), msg.getPartitionNo(), 10000));
+
+		return m_taskQueues.get(tp).submit(msg);
 	}
 
-	private ProducerEndpointWorker createWorkerIfNecessary(String topic, int partition) {
-		Pair<String, Integer> key = new Pair<>(topic, partition);
-		if (!m_workers.containsKey(key)) {
-			synchronized (m_workers) {
-				if (!m_workers.containsKey(key)) {
+	private void startEndpointSender(int taskExecThreadCount, long interval, int batchSize, long timeout) {
+		// TODO
+		Thread t = new Thread(new EndpointSender(taskExecThreadCount, batchSize, timeout, interval));
+		t.setName("ProducerEndpointSender");
+		t.setDaemon(true);
+		t.start();
+	}
 
-					// TODO config batchSize, interval, timeout
-					ProducerEndpointWorker worker = new ProducerEndpointWorker(topic, partition, 3000, 50L, 50L);
+	private class EndpointSender implements Runnable {
 
-					worker.start();
+		private ConcurrentMap<Pair<String, Integer>, AtomicBoolean> m_runnings = new ConcurrentHashMap<>();
 
-					m_workers.put(key, worker);
+		private int m_batchSize;
+
+		private long m_timeout;
+
+		private long m_interval;
+
+		private ExecutorService m_taskExecThreadPool;
+
+		public EndpointSender(int taskExecThreadCount, int batchSize, long timeout, long interval) {
+			m_batchSize = batchSize;
+			m_timeout = timeout;
+			m_interval = interval;
+
+			m_taskExecThreadPool = Executors.newFixedThreadPool(taskExecThreadCount, new ThreadFactory() {
+
+				@Override
+				public Thread newThread(Runnable r) {
+					return new Thread(r, "SenderTask");
+				}
+			});
+		}
+
+		@Override
+		public void run() {
+			while (!Thread.currentThread().isInterrupted()) {
+				for (Map.Entry<Pair<String, Integer>, TaskQueue> entry : m_taskQueues.entrySet()) {
+					try {
+						TaskQueue queue = entry.getValue();
+
+						if (queue.hasTask()) {
+							scheduleTaskExecution(entry.getKey(), queue);
+						}
+					} catch (Exception e) {
+						// TODO
+						e.printStackTrace();
+					}
+				}
+
+				try {
+					TimeUnit.MILLISECONDS.sleep(m_interval);
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
 				}
 			}
 		}
-		return m_workers.get(key);
+
+		private void scheduleTaskExecution(Pair<String, Integer> key, TaskQueue queue) {
+			m_runnings.putIfAbsent(key, new AtomicBoolean(false));
+
+			if (m_runnings.get(key).compareAndSet(false, true)) {
+				m_taskExecThreadPool.submit(new SendTask(key.getKey(), key.getValue(), queue, m_batchSize, m_timeout,
+				      m_runnings.get(key)));
+			}
+		}
+
 	}
 
-	private class ProducerEndpointWorker extends Thread {
-
-		private BlockingQueue<ProducerWorkerContext> m_queue = new LinkedBlockingQueue<>();
+	private class SendTask implements Runnable {
 
 		private String m_topic;
 
 		private int m_partition;
 
-		private int m_batchSize;
+		private TaskQueue m_taskQueue;
 
-		private long m_interval;
+		private int m_batchSize;
 
 		private long m_timeout;
 
-		public ProducerEndpointWorker(String topic, int partition, int batchSize, long interval, long timeout) {
+		private AtomicBoolean m_running;
+
+		public SendTask(String topic, int partition, TaskQueue taskQueue, int batchSize, long timeout,
+		      AtomicBoolean running) {
 			m_topic = topic;
 			m_partition = partition;
+			m_taskQueue = taskQueue;
 			m_batchSize = batchSize;
-			m_interval = interval;
 			m_timeout = timeout;
-			setDaemon(true);
-			setName(String.format("ProducerEndpointWorker-%s-%s", topic, partition));
-		}
-
-		public Future<SendResult> submit(ProducerMessage<?> msg) {
-			SettableFuture<SendResult> future = SettableFuture.create();
-
-			m_queue.offer(new ProducerWorkerContext(msg, future));
-
-			return future;
+			m_running = running;
 		}
 
 		@Override
 		public void run() {
-			SendMessageCommand cmd = null;
 
-			while (!Thread.interrupted()) {
-				try {
-					if (cmd == null) {
-						cmd = createSendMessageCommand();
-					}
+			try {
 
-					if (cmd != null && sendMessagesToBroker(cmd)) {
-						cmd = null;
-					}
+				SendMessageCommand cmd = m_taskQueue.peek(m_batchSize);
 
-				} catch (InterruptedException e) {
-					Thread.currentThread().interrupt();
-				} catch (Exception e) {
-					// TODO
-					e.printStackTrace();
-				} finally {
-					try {
-						TimeUnit.MILLISECONDS.sleep(m_interval);
-					} catch (InterruptedException e) {
-						Thread.currentThread().interrupt();
-					}
+				if (cmd != null && sendMessagesToBroker(cmd)) {
+					m_taskQueue.pop();
 				}
+
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			} catch (Exception e) {
+				// TODO
+				e.printStackTrace();
+			} finally {
+				m_running.set(false);
 			}
+
 		}
 
 		private boolean sendMessagesToBroker(SendMessageCommand cmd) throws InterruptedException, ExecutionException,
@@ -138,11 +190,38 @@ public class BrokerMessageSender extends AbstractMessageSender implements Messag
 				return false;
 			}
 		}
+	}
 
-		private SendMessageCommand createSendMessageCommand() {
+	private class TaskQueue {
+		private String m_topic;
+
+		private int m_partition;
+
+		private AtomicReference<SendMessageCommand> m_cmd = new AtomicReference<>(null);
+
+		private BlockingQueue<ProducerWorkerContext> m_queue;
+
+		public TaskQueue(String topic, int partition, int queueSize) {
+			m_topic = topic;
+			m_partition = partition;
+			m_queue = new LinkedBlockingQueue<>(queueSize);
+		}
+
+		public void pop() {
+			m_cmd.set(null);
+		}
+
+		public SendMessageCommand peek(int size) {
+			if (m_cmd.get() == null) {
+				m_cmd.set(createSendMessageCommand(size));
+			}
+			return m_cmd.get();
+		}
+
+		private SendMessageCommand createSendMessageCommand(int size) {
 			SendMessageCommand cmd = null;
 			List<ProducerWorkerContext> contexts = new ArrayList<>();
-			m_queue.drainTo(contexts, m_batchSize);
+			m_queue.drainTo(contexts, size);
 			if (!contexts.isEmpty()) {
 				cmd = new SendMessageCommand(m_topic, m_partition);
 				for (ProducerWorkerContext context : contexts) {
@@ -152,16 +231,28 @@ public class BrokerMessageSender extends AbstractMessageSender implements Messag
 			return cmd;
 		}
 
-		private class ProducerWorkerContext {
-			private ProducerMessage<?> m_msg;
+		public boolean hasTask() {
+			return m_cmd.get() != null || !m_queue.isEmpty();
+		}
 
-			private SettableFuture<SendResult> m_future;
+		public Future<SendResult> submit(ProducerMessage<?> msg) {
+			SettableFuture<SendResult> future = SettableFuture.create();
 
-			public ProducerWorkerContext(ProducerMessage<?> msg, SettableFuture<SendResult> future) {
-				m_msg = msg;
-				m_future = future;
-			}
+			m_queue.offer(new ProducerWorkerContext(msg, future));
 
+			return future;
+		}
+
+	}
+
+	private static class ProducerWorkerContext {
+		private ProducerMessage<?> m_msg;
+
+		private SettableFuture<SendResult> m_future;
+
+		public ProducerWorkerContext(ProducerMessage<?> msg, SettableFuture<SendResult> future) {
+			m_msg = msg;
+			m_future = future;
 		}
 
 	}
