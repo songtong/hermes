06de43241823b9e972103a1256c9eb61d99745a2
marsqing <q_gu@ctrip.com>
2015-05-22 15:17:45 +0800

    ensure cat client.xml exists when start broker

--- hermes-broker/src/main/java/com/ctrip/hermes/broker/BrokerServer.java
+++ hermes-broker/src/main/java/com/ctrip/hermes/broker/BrokerServer.java
@@ -1,10 +1,19 @@
 package com.ctrip.hermes.broker;
 
+import java.io.File;
+
 import com.ctrip.hermes.broker.bootstrap.BrokerBootstrap;
 import com.ctrip.hermes.core.utils.PlexusComponentLocator;
+import com.dianping.cat.Cat;
 
 public class BrokerServer {
 	public static void main(String[] args) throws Exception {
+		File catConfigFile = new File("/data/appdatas/cat/client.xml");
+		if (!catConfigFile.isFile() || !catConfigFile.canRead()) {
+			throw new RuntimeException(String.format("Cat config file %s not found", catConfigFile.getCanonicalPath()));
+		}
+
+		Cat.initialize(catConfigFile);
 		PlexusComponentLocator.lookup(BrokerBootstrap.class).start();
 	}
 }
