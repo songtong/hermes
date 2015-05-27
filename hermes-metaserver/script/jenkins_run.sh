4b930fd249b265c4e236980bc883d8abdab8ee55
Yiming Liu <liuyiming.vip@gmail.com>
2015-05-27 14:13:25 +0800

    Update jenkins script

--- hermes-metaserver/script/jenkins_run.sh
+++ hermes-metaserver/script/jenkins_run.sh
@@ -5,10 +5,11 @@ set -u
 APP_PREFIX="metaserver"
 APP_PATH="/opt/ctrip/app/hermes-${APP_PREFIX}"
 
-# copy tar
-mkdir ${APP_PATH}/old-deployment
-mv ${APP_PATH}/*.jar ${APP_PATH}/old-deployment
-mv ${APP_PATH}/*.sh ${APP_PATH}/old-deployment
+# copy jar
+now=$(date +"%F_%T")
+mkdir -p ${APP_PATH}/${now}
+mv ${APP_PATH}/*.jar ${APP_PATH}/$now/
+mv ${APP_PATH}/*.sh ${APP_PATH}/$now/
 cp hermes-${APP_PREFIX}/target/*.jar ${APP_PATH}
 cp hermes-${APP_PREFIX}/script/*.sh ${APP_PATH}
 
