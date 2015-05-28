0001d7a1f581ba109c09256de003be5567d9a278
ske <ske@Ctrip.com>
2015-05-28 13:47:17 +0800

    remove useless hermes-rest properties.

--- hermes-rest/src/main/java/com/ctrip/hermes/rest/HermesRestServer.java
+++ hermes-rest/src/main/java/com/ctrip/hermes/rest/HermesRestServer.java
@@ -50,7 +50,6 @@ public class HermesRestServer {
 	}
 
 	public void init() {
-		Configuration.addResource("hermes-rest-default.properties"); // first load common & default properties
 		Configuration.addResource("hermes-rest.properties"); // then load env related properties
 
 		this.port = Configuration.getInt("server.port", 1357);
