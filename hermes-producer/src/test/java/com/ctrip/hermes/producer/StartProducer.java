5d290b7223246b9ef47c6131a6b54530cd760301
Leo Liang <liangjinhua@gmail.com>
2015-06-02 13:57:10 +0800

    refine startProducer

--- hermes-producer/src/test/java/com/ctrip/hermes/producer/StartProducer.java
+++ hermes-producer/src/test/java/com/ctrip/hermes/producer/StartProducer.java
@@ -4,17 +4,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.junit.Test;
 import org.unidal.lookup.ComponentTestCase;
 
+import com.ctrip.hermes.core.result.SendResult;
 import com.ctrip.hermes.producer.api.Producer;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.SettableFuture;
 
 /**
  * @author Leo Liang(jhliang@ctrip.com)
  *
  */
 public class StartProducer extends ComponentTestCase {
+	private ExecutorService m_executors = Executors.newFixedThreadPool(5);
 
 	@Test
 	public void test() throws Exception {
@@ -35,16 +42,30 @@ public class StartProducer extends ComponentTestCase {
 
 	private void send(String topic) throws Exception {
 		String uuid = UUID.randomUUID().toString();
-		String msg = uuid;
 		Random random = new Random();
 
 		boolean priority = random.nextBoolean();
-		msg += priority ? " priority" : " non-priority";
+		final String msg = uuid + (priority ? " priority" : " non-priority");
 		System.out.println(">>> " + msg);
+		SettableFuture<SendResult> future = null;
 		if (priority) {
-			Producer.getInstance().message(topic, uuid, msg).withRefKey(uuid).withPriority().send();
+			future = (SettableFuture<SendResult>) Producer.getInstance().message(topic, uuid, msg).withRefKey(uuid).withPriority().send();
 		} else {
-			Producer.getInstance().message(topic, uuid, msg).withRefKey(uuid).send();
+			future = (SettableFuture<SendResult>) Producer.getInstance().message(topic, uuid, msg).withRefKey(uuid).send();
 		}
+
+		Futures.addCallback(future, new FutureCallback<SendResult>() {
+
+			@Override
+			public void onSuccess(SendResult result) {
+				System.out.println("Message " + msg + " sent.");
+			}
+
+			@Override
+			public void onFailure(Throwable t) {
+				System.out.println("Message " + msg + " sent.");
+
+			}
+		}, m_executors);
 	}
 }
