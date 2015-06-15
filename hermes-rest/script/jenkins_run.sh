c1136243214fd68a02ba00c7f0a85a20c3eee30c
ske <ske@Ctrip.com>
2015-06-15 14:44:11 +0800

    add deploy scripts for jenkins

--- hermes-rest/script/jenkins_run.sh
+++ hermes-rest/script/jenkins_run.sh
@@ -6,14 +6,14 @@ APP_PREFIX="rest"
 APP_PATH="/opt/ctrip/app/hermes-${APP_PREFIX}"
 
 # copy tar
-mkdir ${APP_PATH}/old-deployment
+mkdir -p ${APP_PATH}/old-deployment
 mv ${APP_PATH}/*.tar ${APP_PATH}/old-deployment
 cp hermes-${APP_PREFIX}/target/*.tar ${APP_PATH}
 
 
 cd ${APP_PATH}
-rm -rf `ls | grep -v ".*${APP_PREFIX}.*tar"`
-tar -xf  *${APP_PREFIX}*.tar
+echo "Remove [" `ls | grep -v ".*${APP_PREFIX}.*tar\|old-deployment"`"]"
+rm -rf `ls | grep -v ".*${APP_PREFIX}.*tar\|old-deployment"`
 cd h*/bin
 bash ./startup.sh stop
 bash ./startup.sh start
