6c5645800096df06a8fc473f140843a7b8c92ec5
ske <ske@Ctrip.com>
2015-07-22 17:03:37 +0800

    add cat.xml into rest

--- hermes-rest/src/main/java/com/ctrip/hermes/rest/common/GatewayListener.java
+++ hermes-rest/src/main/java/com/ctrip/hermes/rest/common/GatewayListener.java
@@ -1,10 +1,14 @@
 package com.ctrip.hermes.rest.common;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import com.ctrip.hermes.core.utils.PlexusComponentLocator;
 import com.ctrip.hermes.rest.service.SubscriptionRegisterService;
+import com.dianping.cat.Cat;
 import com.yammer.metrics.Metrics;
 
 public class GatewayListener implements ServletContextListener {
@@ -14,6 +18,18 @@ public class GatewayListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		File catConfigFile = new File("/opt/ctrip/data/cat/client.xml");
+		if (!catConfigFile.isFile() || !catConfigFile.canRead()) {
+			try {
+				throw new IllegalStateException(
+						  String.format("Cat config file %s not found", catConfigFile.getCanonicalPath()));
+			} catch (IOException e) {
+				// todo: handle exception
+				e.printStackTrace();
+			}
+		}
+		Cat.initialize(catConfigFile);
+
 		subsribeRegistry.start();
 	}
 
