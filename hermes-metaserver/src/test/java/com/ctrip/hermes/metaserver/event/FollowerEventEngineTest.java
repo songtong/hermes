90d99695965e1c80c2d264ef13fd901b1cc98c28
Leo Liang <liangjinhua@gmail.com>
2015-07-30 19:06:18 +0800

    change broker project to war

---
+++ hermes-metaserver/src/test/java/com/ctrip/hermes/metaserver/event/FollowerEventEngineTest.java
@@ -0,0 +1,17 @@
+package com.ctrip.hermes.metaserver.event;
+
+import com.ctrip.hermes.metaserver.ZKSuppportTestCase;
+
+/**
+ * @author Leo Liang(jhliang@ctrip.com)
+ *
+ */
+public class FollowerEventEngineTest extends ZKSuppportTestCase {
+
+	@Override
+   protected void initZkData() throws Exception {
+	   // TODO Auto-generated method stub
+	   
+   }
+
+}
