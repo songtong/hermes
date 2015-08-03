695427d318821355ebec98216084ae4f3b0143a7
h2o68 <563239103@qq.com>
2015-08-03 14:25:12 +0800

    bug_fixed

--- hermes-portal/src/main/java/com/ctrip/hermes/portal/web/HermesValidationFilter.java
+++ hermes-portal/src/main/java/com/ctrip/hermes/portal/web/HermesValidationFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import com.ctrip.hermes.core.utils.PlexusComponentLocator;
@@ -29,19 +28,28 @@ public class HermesValidationFilter implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 		boolean isLogined = false;
 		if (req.getCookies() != null) {
-			for (Cookie cookie : req.getCookies()) {
-				if ("_token".equals(cookie.getName())) {
-					try {
-						isLogined = validateCookie(cookie.getValue(), request.getRemoteAddr());
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					break;
-				}
+			try {
+				isLogined = validateCookie(getToken(req), request.getRemoteAddr());
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
 		request.setAttribute("logined", isLogined);
 		chain.doFilter(request, response);
+	}
+
+	private String getToken(HttpServletRequest request) {
+		String header = request.getHeader("Cookie");
+		for (String part : header.split("; ")) {
+			int sep = part.indexOf("=");
+			if (sep > 0) {
+				String cookieName = part.substring(0, sep);
+				if (cookieName.equals("_token")) {
+					return part.substring(sep + 1);
+				}
+			}
+		}
+		return "";
 	}
 
 	private boolean validateCookie(String cookie, String ip) throws Exception {
