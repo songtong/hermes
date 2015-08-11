de9a416a6fa5936f3bea4a177e55ed447114da3c
Leo Liang <liangjinhua@gmail.com>
2015-08-11 18:41:39 +0800

    for test

--- hermes-metaserver/src/main/java/com/ctrip/hermes/metaserver/event/impl/BaseEventHandler.java
+++ hermes-metaserver/src/main/java/com/ctrip/hermes/metaserver/event/impl/BaseEventHandler.java
@@ -1,5 +1,8 @@
 package com.ctrip.hermes.metaserver.event.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ctrip.hermes.metaserver.cluster.ClusterStateHolder;
 import com.ctrip.hermes.metaserver.event.Event;
 import com.ctrip.hermes.metaserver.event.EventBus;
@@ -11,12 +14,14 @@ import com.ctrip.hermes.metaserver.event.EventHandler;
  *
  */
 public abstract class BaseEventHandler implements EventHandler {
+	private static final Logger log = LoggerFactory.getLogger(BaseEventHandler.class);
 
 	@Override
 	public void onEvent(EventEngineContext context, Event event) throws Exception {
 		EventBus eventBus = context.getEventBus();
 		ClusterStateHolder clusterStateHolder = context.getClusterStateHolder();
 
+		log.info("[FOR_TEST] event handler triggered {} {}.", this.eventType(), this.getName());
 		if (eventBus.isStopped()) {
 			return;
 		}
@@ -29,6 +34,7 @@ public abstract class BaseEventHandler implements EventHandler {
 			return;
 		}
 
+		log.info("[FOR_TEST] process event {} {}.", this.eventType(), this.getName());
 		processEvent(context, event);
 	}
 
--- hermes-metaserver/src/main/java/com/ctrip/hermes/metaserver/event/impl/BrokerListChangedEventHandler.java
+++ hermes-metaserver/src/main/java/com/ctrip/hermes/metaserver/event/impl/BrokerListChangedEventHandler.java
@@ -2,6 +2,8 @@ package com.ctrip.hermes.metaserver.event.impl;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.unidal.lookup.annotation.Inject;
 import org.unidal.lookup.annotation.Named;
 
@@ -20,6 +22,8 @@ import com.ctrip.hermes.metaserver.meta.MetaHolder;
  */
 @Named(type = EventHandler.class, value = "BrokerListChangedEventHandler")
 public class BrokerListChangedEventHandler extends BaseEventHandler {
+	private static final Logger log = LoggerFactory.getLogger(BrokerListChangedEventHandler.class);
+
 	@Inject
 	private BrokerAssignmentHolder m_brokerAssignmentHolder;
 
@@ -42,6 +46,7 @@ public class BrokerListChangedEventHandler extends BaseEventHandler {
 			Map<String, ClientContext> brokers = (Map<String, ClientContext>) data;
 
 			m_brokerAssignmentHolder.reassign(brokers);
+			log.info("[FOR_TEST] Broker assignment reassigned, since broker list changed.");
 			m_metaHolder.update(m_endpointMaker.makeEndpoints(context, m_brokerAssignmentHolder.getAssignments()));
 		}
 	}
