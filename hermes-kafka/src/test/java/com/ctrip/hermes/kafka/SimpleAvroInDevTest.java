3712a55190cf91083308d329d2630d8d8fcac9ee
Yiming Liu <liuyiming.vip@gmail.com>
2015-09-07 12:08:32 +0800

    Update kafka config

--- hermes-kafka/src/test/java/com/ctrip/hermes/kafka/SimpleAvroInDevTest.java
+++ hermes-kafka/src/test/java/com/ctrip/hermes/kafka/SimpleAvroInDevTest.java
@@ -6,61 +6,28 @@ import java.io.InputStreamReader;
 
 import org.junit.Test;
 
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
 import com.ctrip.hermes.consumer.api.BaseMessageListener;
 import com.ctrip.hermes.consumer.api.Consumer;
 import com.ctrip.hermes.consumer.api.Consumer.ConsumerHolder;
 import com.ctrip.hermes.core.message.ConsumerMessage;
 import com.ctrip.hermes.kafka.avro.AvroVisitEvent;
 import com.ctrip.hermes.kafka.avro.KafkaAvroTest;
+import com.ctrip.hermes.metrics.HermesMetricsRegistry;
+import com.ctrip.hermes.metrics.MetricsUtils;
 import com.ctrip.hermes.producer.api.Producer;
 import com.ctrip.hermes.producer.api.Producer.MessageHolder;
 
-public class OneBoxInDevTest {
-
-	@Test
-	public void simpleTextMessageTest() throws IOException {
-		String topic = "kafka.SimpleTextTopic";
-		String group = "simpleTextMessageTest";
-
-		Producer producer = Producer.getInstance();
-
-		ConsumerHolder consumer = Consumer.getInstance().start(topic, group, new BaseMessageListener<String>() {
-
-			@Override
-			protected void onMessage(ConsumerMessage<String> msg) {
-				String body = msg.getBody();
-				System.out.println("Receive: " + body);
-			}
-		});
-
-		System.out.println("Starting consumer...");
-
-		boolean hasHeader = true;
-		try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
-			while (true) {
-				String line = in.readLine();
-				if ("q".equals(line)) {
-					break;
-				}
-
-				String proMsg = "Hello Ctrip " + System.currentTimeMillis();
-				MessageHolder holder = producer.message(topic, null, proMsg);
-				if (!hasHeader) {
-					holder = holder.withoutHeader();
-				}
-				hasHeader = !hasHeader;
-				holder.send();
-				System.out.println("Sent: " + proMsg);
-			}
-		}
-
-		consumer.close();
-	}
+public class SimpleAvroInDevTest {
 
 	@Test
 	public void simpleAvroMessageTest() throws IOException {
 		String topic = "kafka.SimpleAvroTopic";
 		String group = "simpleTextMessageTest";
+		MetricRegistry metrics = HermesMetricsRegistry.getMetricRegistryByT(topic);
+		final Meter sent = metrics.meter("sent");
+		final Meter received = metrics.meter("received");
 
 		Producer producer = Producer.getInstance();
 
@@ -70,6 +37,7 @@ public class OneBoxInDevTest {
 			protected void onMessage(ConsumerMessage<AvroVisitEvent> msg) {
 				AvroVisitEvent body = msg.getBody();
 				System.out.println("Receive: " + body);
+				received.mark();
 			}
 		});
 
@@ -84,16 +52,19 @@ public class OneBoxInDevTest {
 				}
 
 				AvroVisitEvent proMsg = KafkaAvroTest.generateEvent();
-				MessageHolder holder = producer.message(topic, null, proMsg);
+				MessageHolder holder = producer.message(topic, proMsg.getIp().toString(), proMsg);
 				if (!hasHeader) {
 					holder = holder.withoutHeader();
 				}
 				hasHeader = !hasHeader;
 				holder.send();
 				System.out.println("Sent: " + proMsg);
+				sent.mark();
 			}
 		}
 
 		consumer.close();
+		System.out.println(MetricsUtils.printMeter("sent", sent));
+		System.out.println(MetricsUtils.printMeter("received", received));
 	}
 }
