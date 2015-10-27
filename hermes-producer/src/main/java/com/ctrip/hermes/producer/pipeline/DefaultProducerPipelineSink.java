65751b89ae75d44e16c12cad8230598137e34a31
marsqing <q_gu@ctrip.com>
2015-10-27 17:44:25 +0800

    log hermes client version to cat

--- hermes-producer/src/main/java/com/ctrip/hermes/producer/pipeline/DefaultProducerPipelineSink.java
+++ hermes-producer/src/main/java/com/ctrip/hermes/producer/pipeline/DefaultProducerPipelineSink.java
@@ -4,11 +4,13 @@ import java.util.concurrent.Future;
 
 import org.unidal.lookup.annotation.Inject;
 
+import com.ctrip.hermes.Hermes;
 import com.ctrip.hermes.core.message.ProducerMessage;
 import com.ctrip.hermes.core.pipeline.PipelineContext;
 import com.ctrip.hermes.core.pipeline.PipelineSink;
 import com.ctrip.hermes.core.result.SendResult;
 import com.ctrip.hermes.producer.sender.MessageSender;
+import com.dianping.cat.Cat;
 
 public class DefaultProducerPipelineSink implements PipelineSink<Future<SendResult>> {
 	@Inject
@@ -16,7 +18,11 @@ public class DefaultProducerPipelineSink implements PipelineSink<Future<SendResu
 
 	@Override
 	public Future<SendResult> handle(PipelineContext<Future<SendResult>> ctx, Object input) {
+		logVersionToCat();
 		return messageSender.send((ProducerMessage<?>) input);
+	}
 
+	private void logVersionToCat() {
+		Cat.logEvent("Hermes.Client.Version", Hermes.VERSION);
 	}
 }
