e803954f078e49f965cd3be862465b12221c1a1e
songtong <tong.song@live.com>
2015-10-30 12:39:28 +0800

    broker checker & meta checker

---
+++ hermes-monitor/src/main/java/com/ctrip/hermes/monitor/checker/server/ServerCheckerConstans.java
@@ -0,0 +1,9 @@
+package com.ctrip.hermes.monitor.checker.server;
+
+public class ServerCheckerConstans {
+	public static final String ES_INDEX = "logstash-*";
+
+	public static final String ES_DOC_TYPE_BROKER = "broker";
+
+	public static final String ES_DOC_TYPE_METASERVER = "metaserver";
+}
