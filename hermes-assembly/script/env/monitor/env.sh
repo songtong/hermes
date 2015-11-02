dc8a30e1edd7e69d9229be7fd2c373470a85f7b5
Yiming Liu <liuyiming.vip@gmail.com>
2015-11-02 17:31:02 +0800

    Add Monitor Deploy Script

---
+++ hermes-assembly/script/env/monitor/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+LOG_PATH=/opt/logs/hermes-monitor/
+JMX_PORT=8307
+STOP_PORT=9307
--- hermes-monitor/src/main/java/com/ctrip/hermes/monitor/Bootstrap.java
+++ hermes-monitor/src/main/java/com/ctrip/hermes/monitor/Bootstrap.java
@@ -2,6 +2,8 @@ package com.ctrip.hermes.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -10,7 +12,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-public class Bootstrap {
+public class Bootstrap extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Bootstrap.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bootstrap.class);
