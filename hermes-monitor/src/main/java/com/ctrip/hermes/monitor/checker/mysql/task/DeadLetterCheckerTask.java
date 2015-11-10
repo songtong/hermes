5e0bee7c38ed8dca5baff677042155f64cd68b80
songtong <tong.song@live.com>
2015-11-10 16:31:23 +0800

    filter kafka topics when check dead-letter & consume-backlog

--- hermes-monitor/src/main/java/com/ctrip/hermes/monitor/checker/mysql/task/DeadLetterCheckerTask.java
+++ hermes-monitor/src/main/java/com/ctrip/hermes/monitor/checker/mysql/task/DeadLetterCheckerTask.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.unidal.dal.jdbc.DalException;
 
 import com.ctrip.hermes.meta.entity.Partition;
+import com.ctrip.hermes.meta.entity.Storage;
 import com.ctrip.hermes.meta.entity.Topic;
 import com.ctrip.hermes.metaservice.monitor.event.TopicLargeDeadLetterEvent;
 import com.ctrip.hermes.metaservice.queue.DeadLetterDao;
@@ -50,21 +51,23 @@ public class DeadLetterCheckerTask implements Runnable {
 	public void run() {
 		try {
 			for (Entry<Topic, Integer> entry : m_limits.entrySet()) {
-				String topic = entry.getKey().getName();
-				long tpDeadSum = 0;
-				for (Partition partition : entry.getKey().getPartitions()) {
-					try {
-						tpDeadSum += m_dao.findByTimeRange( //
-						      topic, partition.getId(), m_from, m_to, DeadLetterEntity.READSET_COUNT) //
-						      .getCountOfTimeRange();
-					} catch (DalException e) {
-						if (log.isDebugEnabled()) {
-							log.debug("Query dead letter count failed, {} {}", topic, partition.getId());
+				if (Storage.MYSQL.equals(entry.getKey().getStorageType())) {
+					String topic = entry.getKey().getName();
+					long tpDeadSum = 0;
+					for (Partition partition : entry.getKey().getPartitions()) {
+						try {
+							tpDeadSum += m_dao.findByTimeRange( //
+							      topic, partition.getId(), m_from, m_to, DeadLetterEntity.READSET_COUNT) //
+							      .getCountOfTimeRange();
+						} catch (DalException e) {
+							if (log.isDebugEnabled()) {
+								log.debug("Query dead letter count failed, {} {}", topic, partition.getId());
+							}
 						}
 					}
-				}
-				if (tpDeadSum >= entry.getValue()) {
-					m_result.addMonitorEvent(new TopicLargeDeadLetterEvent(topic, tpDeadSum, m_from, m_to));
+					if (tpDeadSum >= entry.getValue()) {
+						m_result.addMonitorEvent(new TopicLargeDeadLetterEvent(topic, tpDeadSum, m_from, m_to));
+					}
 				}
 			}
 		} catch (Exception e) {
