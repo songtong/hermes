bad07e120c00332495fa72d146cd972035fc9d26
h2o68 <563239103@qq.com>
2015-12-11 16:08:20 +0800

    fix bug

--- hermes-portal/src/main/java/com/ctrip/hermes/portal/service/application/ApplicationService.java
+++ hermes-portal/src/main/java/com/ctrip/hermes/portal/service/application/ApplicationService.java
@@ -18,7 +18,7 @@ public interface ApplicationService {
 
 	public List<HermesApplication> getApplicationsByStatus(int Status);
 
-	public TopicView generageTopicView(TopicApplication app);
+	public TopicView generateTopicView(TopicApplication app);
 
 	public ConsumerView generateConsumerView(ConsumerApplication app);
 
