b22223c5dfa8b19a6b9a4a03b15fba2ef8d8ef46
Leo Liang <liangjinhua@gmail.com>
2016-02-23 13:25:39 +0800

    ack holder concurrent issue bug fix and bump to 0.6.6

--- hermes-core/src/main/java/com/ctrip/hermes/Hermes.java
+++ hermes-core/src/main/java/com/ctrip/hermes/Hermes.java
@@ -2,7 +2,7 @@ package com.ctrip.hermes;
 
 public class Hermes {
 
-	public final static String VERSION = "java-0.6.5";
+	public final static String VERSION = "java-0.6.6";
 
 	private static Env m_env;
 
