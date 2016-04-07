de47c29d42c45db96c8975f3b33b191a3875347e
marsqing <q_gu@ctrip.com>
2016-04-07 15:04:55 +0800

    replace jetty with tomcat and support local profile

---
+++ hermes-assembly/script/env/broker/local/env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOG_PATH=/opt/logs/hermes-broker/
+JMX_PORT=8301
+STOP_PORT=9301
+STOP_TIMEOUT=30
+# set jvm startup argument
+JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true \
+            -Dfile.encoding=utf-8 \
+            -Dio.netty.allocator.type=pooled \
+            -Xms4g \
+            -Xmx4g \
+            -XX:PermSize=256m \
+            -XX:MaxPermSize=256m \
+            -XX:MaxDirectMemorySize=4g \
+            -XX:+PrintGC \
+            -XX:+PrintGCDetails \
+            -XX:+PrintGCDateStamps \
+            -Xloggc:$LOG_PATH/gc.log \
+            -XX:-OmitStackTraceInFastThrow \
+            -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$LOG_PATH
+            "
+export JAVA_OPTS=${JAVA_OPTS}
