ccf69fddb149e810c175d34a8e2d4a147ef14e10
Leo Liang <liangjinhua@gmail.com>
2016-04-29 15:10:28 +0800

    bug fix

--- hermes-broker/src/main/java/com/ctrip/hermes/broker/transport/command/processor/SendMessageCommandProcessorV3.java
+++ hermes-broker/src/main/java/com/ctrip/hermes/broker/transport/command/processor/SendMessageCommandProcessorV3.java
@@ -183,7 +183,7 @@ public class SendMessageCommandProcessorV3 implements CommandProcessor {
 					if (m_written.compareAndSet(false, true)) {
 						logToCatIfHasError(m_result);
 						m_result.getHeader().addProperty("createTime", Long.toString(System.currentTimeMillis()));
-						m_ctx.write(m_result);
+						ChannelUtils.writeAndFlush(m_ctx.getChannel(), m_result);
 
 						logElapse();
 					}
