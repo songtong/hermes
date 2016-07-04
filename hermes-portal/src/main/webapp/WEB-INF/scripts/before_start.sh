a9f46821d0fb9fb0c44c3743b56bcc1afcab2faf
lxteng <lxteng@ctrip.com>
2016-07-04 11:16:49 +0800

    backup codes

--- hermes-portal/src/main/webapp/WEB-INF/scripts/before_start.sh
+++ hermes-portal/src/main/webapp/WEB-INF/scripts/before_start.sh
@@ -28,6 +28,19 @@ if [[ -e $DATA_DIR ]]; then
 	mkdir -p $DATA_DIR
 fi
 
+# Replace web.xml
+cp $SCRIPT_DIR/../tars/web.xml $SCRIPT_DIR/../web.xml
+
+# Replace parameters into web.xml
+. $SCRIPT_DIR/../tars/$ENV/sso.properties
+sed -i "s/credisServiceUrl/$credisServiceUrl/g" $SCRIPT_DIR/../web.xml
+sed -i "s/casServerLoginUrl/$casServerLoginUrl/g" $SCRIPT_DIR/../web.xml
+sed -i "s/casServerUrlPrefix/$casServerUrlPrefix/g" $SCRIPT_DIR/../web.xml
+sed -i "s/serverName/$serverName/g" $SCRIPT_DIR/../web.xml
+
+# Replace hermes.properties file.
+cp $SCRIPT_DIR/../tars/$ENV/hermes.properties $SCRIPT_DIR/../classes/
+
 # Distribute list of config files.
 cp $SCRIPT_DIR/../tars/$ENV/datasources.xml $DATA_DIR/datasources.xml
 cp $SCRIPT_DIR/../tars/mail.properties $DATA_DIR/mail.properties
