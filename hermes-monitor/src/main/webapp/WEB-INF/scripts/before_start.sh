52a217763e060e038235914ff8b2ca6f43c4c5e2
lxteng <lxteng@ctrip.com>
2016-07-15 14:56:52 +0800

    change/add scripts for tars release

--- hermes-monitor/src/main/webapp/WEB-INF/scripts/before_start.sh
+++ hermes-monitor/src/main/webapp/WEB-INF/scripts/before_start.sh
@@ -12,28 +12,25 @@ APP_ID=100003808
 # Define app name.
 APP_NAME=monitor
 
-# Define data dir for app.
-DATA_DIR=/opt/data/${APP_ID}
-
-# Define source dir.
-SOURCE_DIR=${DATA_DIR}/hermes-config/${APP_NAME}
-
 # Define hermes dir.
 CONFIG_DIR=/opt/data/hermes
+
+# Define source dir.
+SOURCE_DIR=$CONFIG_DIR/hermes-config/${APP_NAME}
 
 # Solid server settings position.
 SERVER_SETTINGS=/opt/settings/server.properties
 
 # Clone or pull git config project.
-if [[ ! -e $DATA_DIR ]]; then
-	mkdir -p $DATA_DIR
+if [[ ! -e $CONFIG_DIR ]]; then
+	mkdir -p $CONFIG_DIR
 fi
 
-if [[ ! -e $DATA_DIR/hermes-config ]]; then
-	git clone http://git.dev.sh.ctripcorp.com/hermes/hermes-config.git ${DATA_DIR}/hermes-config
+if [[ ! -e $CONFIG_DIR/hermes-config ]]; then
+	cd $CONFIG_DIR && git clone http://git.dev.sh.ctripcorp.com/hermes/hermes-config.git $CONFIG_DIR/hermes-config
 fi
 
-cd ${DATA_DIR}/hermes-config && git pull
+cd $CONFIG_DIR/hermes-config && git pull
 
 # Get env param required for config distribution.
 ENV=$(cat $SERVER_SETTINGS | grep env | cut -d'=' -f2 | tr '[:upper:]' '[:lower:]')
@@ -41,11 +38,6 @@ ENV=$(cat $SERVER_SETTINGS | grep env | cut -d'=' -f2 | tr '[:upper:]' '[:lower:
 # Correct env keyword 'pro' to 'prod'.
 if [[ $ENV = 'pro' ]]; then
 	ENV='prod'
-fi
-
-# Create dir if not exists.
-if [[ ! -e $CONFIG_DIR ]]; then
-	mkdir -p $CONFIG_DIR
 fi
 
 # Replace hermes.properties file.
@@ -56,7 +48,3 @@ cp $SOURCE_DIR/$ENV/datasources.xml $CONFIG_DIR/datasources.xml
 cp $SOURCE_DIR/mail.properties $CONFIG_DIR/mail.properties
 cp $SOURCE_DIR/hermes-es.token $CONFIG_DIR/hermes-es.token
 cp $SOURCE_DIR/TTS.TOKEN $CONFIG_DIR/TTS.TOKEN
-
-
-
-
