9cef7641f20b86e7bbd462969c5e5c39a5249497
lxteng <lxteng@ctrip.com>
2016-10-24 16:47:20 +0800

    fix ulimit settings for portal & monitor

--- hermes-portal/src/main/webapp/WEB-INF/scripts/sudo_before_shutdown.sh
+++ hermes-portal/src/main/webapp/WEB-INF/scripts/sudo_before_shutdown.sh
@@ -2,3 +2,13 @@
 
 # Top secret
 gpasswd -a deploy admin
+
+# Define app id.
+APP_ID=100003806
+
+if grep -Fq "LimitNOFILE" /usr/lib/systemd/system/ctripapp\@${APP_ID}.service; then
+       echo "already set LimitNOFILE";
+else
+       sed -i '/\[Service\]/a\LimitNOFILE=65536' /usr/lib/systemd/system/ctripapp\@${APP_ID}.service;
+       systemctl daemon-reload
+fi
