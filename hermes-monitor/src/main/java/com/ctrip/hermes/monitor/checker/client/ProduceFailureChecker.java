3f4bcda353c64767c18309be3f2e6529d6b6759f
杨青 <563239103@qq.com>
2016-12-30 11:32:22 +0800

    remove produce error checker & update prod trifecta url

--- hermes-monitor/src/main/java/com/ctrip/hermes/monitor/checker/client/ProduceFailureChecker.java
+++ hermes-monitor/src/main/java/com/ctrip/hermes/monitor/checker/client/ProduceFailureChecker.java
@@ -1,6 +1,5 @@
 package com.ctrip.hermes.monitor.checker.client;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -9,26 +8,16 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Component;
 import org.unidal.tuple.Pair;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.ctrip.hermes.admin.core.monitor.event.ProduceFailureCountTooLargeEvent;
-import com.ctrip.hermes.core.constants.CatConstants;
 import com.ctrip.hermes.core.utils.StringUtils;
 import com.ctrip.hermes.monitor.checker.CatBasedChecker;
 import com.ctrip.hermes.monitor.checker.CheckerResult;
@@ -58,12 +47,12 @@ public class ProduceFailureChecker extends CatBasedChecker implements Initializi
 
 	@Override
 	protected void doCheck(Timespan timespan, CheckerResult result) throws Exception {
-		String catReportUrl = m_config.getCatBaseUrl()
-		      + String.format(m_config.getCatEventUrlPattern(), CAT_DOMAIN, formatToCatUrlTime(timespan.getStartHour()),
-		            CatConstants.TYPE_MESSAGE_PRODUCE_ERROR);
-		String transactionReportXml = curl(catReportUrl, m_config.getCatConnectTimeout(), m_config.getCatReadTimeout());
-		Map<String, List<Pair<Integer, Integer>>> topic2FailureCountList = extractFailureCountFromXml(transactionReportXml);
-		bizCheck(topic2FailureCountList, timespan, result);
+//		String catReportUrl = m_config.getCatBaseUrl()
+//		      + String.format(m_config.getCatEventUrlPattern(), CAT_DOMAIN, formatToCatUrlTime(timespan.getStartHour()),
+//		            CatConstants.type_produce);
+//		String transactionReportXml = curl(catReportUrl, m_config.getCatConnectTimeout(), m_config.getCatReadTimeout());
+//		Map<String, List<Pair<Integer, Integer>>> topic2FailureCountList = extractFailureCountFromXml(transactionReportXml);
+//		bizCheck(topic2FailureCountList, timespan, result);
 	}
 
 	private void bizCheck(Map<String, List<Pair<Integer, Integer>>> topic2FailureCountList, Timespan timespan,
@@ -103,39 +92,39 @@ public class ProduceFailureChecker extends CatBasedChecker implements Initializi
 	private Map<String, List<Pair<Integer, Integer>>> extractFailureCountFromXml(String xmlContent) throws SAXException,
 	      IOException, ParserConfigurationException, XPathExpressionException {
 		Map<String, List<Pair<Integer, Integer>>> topic2FailureCountList = new HashMap<>();
-
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-		Document doc = documentBuilder.parse(new ByteArrayInputStream(xmlContent.getBytes()));
-
-		XPath xPath = XPathFactory.newInstance().newXPath();
-
-		String allTransactionsExpression = "/event/report[@domain='" + CAT_DOMAIN + "']/machine[@ip='All']/type[@id='"
-		      + CatConstants.TYPE_MESSAGE_PRODUCE_ERROR + "']/name";
-
-		NodeList transactionNodes = (NodeList) xPath.compile(allTransactionsExpression).evaluate(doc,
-		      XPathConstants.NODESET);
-
-		for (int i = 0; i < transactionNodes.getLength(); i++) {
-			Node transactionNode = transactionNodes.item(i);
-
-			String topic = transactionNode.getAttributes().getNamedItem("id").getNodeValue();
-			topic2FailureCountList.put(topic, new LinkedList<Pair<Integer, Integer>>());
-
-			String allRangesExpression = "range";
-
-			NodeList rangeNodes = (NodeList) xPath.compile(allRangesExpression).evaluate(transactionNode,
-			      XPathConstants.NODESET);
-
-			for (int j = 0; j < rangeNodes.getLength(); j++) {
-				Node rangeNode = rangeNodes.item(j);
-				String countStr = rangeNode.getAttributes().getNamedItem("count").getNodeValue();
-				String minuteStr = rangeNode.getAttributes().getNamedItem("value").getNodeValue();
-				topic2FailureCountList.get(topic).add(
-				      new Pair<Integer, Integer>(Integer.parseInt(minuteStr), Integer.parseInt(countStr)));
-			}
-		}
-
+//
+//		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+//		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+//		Document doc = documentBuilder.parse(new ByteArrayInputStream(xmlContent.getBytes()));
+//
+//		XPath xPath = XPathFactory.newInstance().newXPath();
+//
+//		String allTransactionsExpression = "/event/report[@domain='" + CAT_DOMAIN + "']/machine[@ip='All']/type[@id='"
+//		      + CatConstants.TYPE_MESSAGE_PRODUCE_ERROR + "']/name";
+//
+//		NodeList transactionNodes = (NodeList) xPath.compile(allTransactionsExpression).evaluate(doc,
+//		      XPathConstants.NODESET);
+//
+//		for (int i = 0; i < transactionNodes.getLength(); i++) {
+//			Node transactionNode = transactionNodes.item(i);
+//
+//			String topic = transactionNode.getAttributes().getNamedItem("id").getNodeValue();
+//			topic2FailureCountList.put(topic, new LinkedList<Pair<Integer, Integer>>());
+//
+//			String allRangesExpression = "range";
+//
+//			NodeList rangeNodes = (NodeList) xPath.compile(allRangesExpression).evaluate(transactionNode,
+//			      XPathConstants.NODESET);
+//
+//			for (int j = 0; j < rangeNodes.getLength(); j++) {
+//				Node rangeNode = rangeNodes.item(j);
+//				String countStr = rangeNode.getAttributes().getNamedItem("count").getNodeValue();
+//				String minuteStr = rangeNode.getAttributes().getNamedItem("value").getNodeValue();
+//				topic2FailureCountList.get(topic).add(
+//				      new Pair<Integer, Integer>(Integer.parseInt(minuteStr), Integer.parseInt(countStr)));
+//			}
+//		}
+//
 		return topic2FailureCountList;
 	}
 
