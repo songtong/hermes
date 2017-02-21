9e0387ff7e04ff7a273e5f3d88bef986236c2d3e
杨青 <563239103@qq.com>
2017-02-21 11:22:34 +0800

    use encoding method

--- hermes-metaserver/src/main/java/com/ctrip/hermes/metaserver/rest/resource/MetaResource.java
+++ hermes-metaserver/src/main/java/com/ctrip/hermes/metaserver/rest/resource/MetaResource.java
@@ -79,8 +79,8 @@ public class MetaResource {
 		if (!isMetaModified(version, hashCode, meta)) {
 			return Response.status(Status.NOT_MODIFIED).build();
 		}
-		
-		return Response.status(Status.OK).entity(m_metaHolder.getCompressedClientMeta()).header("Content-Encoding", "gzip").build();
+
+		return Response.status(Status.OK).entity(m_metaHolder.getCompressedClientMeta()).encoding("gzip").build();
 	}
 
 	@POST
